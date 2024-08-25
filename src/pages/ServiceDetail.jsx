37c4aff10579e5bb4e1b6d5044a4c3454b68f2f6
Krishna Jaiswal <97112192+Krishna-jaiswal-24@users.noreply.github.com>
2024-08-25 21:53:42 +0530

    Update ServiceDetail.jsx

--- src/pages/ServiceDetail.jsx
+++ src/pages/ServiceDetail.jsx
@@ -35,7 +35,7 @@ const ServiceDetail = () => {
   const toggleAccordion = (index) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
-  window.scrollTo(0, 0);
+
 
   return (
     <section className="px-6 md:px-12 lg:px-24 2xl:px-[20%] py-8 lg:py-16 w-full flex justify-center">
